6fb278060abd90dd5ff9e3dc3bf638061f65a4ce
Khrustalion <409807@niuitmo.ru>
2025-12-20 18:58:57 +0300

    add: memory tests

---
+++ src/test/kotlin/com/compiler/memory/ReferenceCountingGCTes.kt
@@ -0,0 +1,128 @@
+package com.compiler.memory
+
+import com.compiler.bytecode.Value
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ReferenceCountingGCTest {
+
+    @Test
+    fun allocIntArrayHasRefCount1AndFreedAfterRelease() {
+        val mm = MemoryManager()
+
+        val ref = mm.newIntArray(3)
+        assertEquals(1, mm.debugRefCount(ref))
+        assertEquals(1, mm.debugHeapObjectCount())
+
+        mm.release(ref)
+        assertEquals(0, mm.debugHeapObjectCount())
+    }
+
+    @Test
+    fun retainIncrementsAndMultipleReleasesFreeAtZero() {
+        val mm = MemoryManager()
+        val ref = mm.newIntArray(1)
+
+        mm.retain(ref)
+        mm.retain(ref)
+        assertEquals(3, mm.debugRefCount(ref))
+        assertEquals(1, mm.debugHeapObjectCount())
+
+        mm.release(ref)
+        assertEquals(2, mm.debugRefCount(ref))
+
+        mm.release(ref)
+        assertEquals(1, mm.debugRefCount(ref))
+
+        mm.release(ref)
+        assertEquals(0, mm.debugHeapObjectCount())
+    }
+
+    @Test
+    fun stackMoveToLocalsKeepsRefCountStableAndFreesAfterClearingLocals() {
+        val mm = MemoryManager()
+        val stack = RcOperandStack(mm)
+        val locals = RcLocals(mm, size = 1)
+
+        // NEW_ARRAY_INT: получили refCount=1, считаем что это "временный результат инструкции"
+        val ref = mm.newIntArray(5)
+
+        // Положили на стек MOVE (как сделал бы интерпретатор)
+        stack.pushMove(ref)
+        assertEquals(1, mm.debugRefCount(ref))
+
+        // STORE_LOCAL: popMove (ownership передали) -> locals.setMove (ownership у locals)
+        val v = stack.popMove()
+        locals.setMove(0, v)
+        assertEquals(1, mm.debugRefCount(ref))
+
+        // Уберём из locals => release => free
+        locals.clear(0)
+        assertEquals(0, mm.debugHeapObjectCount())
+    }
+
+    @Test
+    fun loadLocalCopiesReferenceAndMustRetain() {
+        val mm = MemoryManager()
+        val stack = RcOperandStack(mm)
+        val locals = RcLocals(mm, size = 1)
+
+        val ref = mm.newIntArray(2)
+        locals.setMove(0, ref) // locals владеет (refCount=1)
+        assertEquals(1, mm.debugRefCount(ref))
+
+        // LOAD_LOCAL должен делать COPY (retain)
+        val copied = locals.getCopy(0)
+        stack.pushMove(copied) // стек принимает владение уже-retain'нутого значения как move
+        assertEquals(2, mm.debugRefCount(ref))
+
+        // POP выбрасывает со стека => release => refCount обратно 1
+        stack.popDrop()
+        assertEquals(1, mm.debugRefCount(ref))
+
+        // очистка locals => free
+        locals.clearAndReleaseAll()
+        assertEquals(0, mm.debugHeapObjectCount())
+    }
+
+    @Test
+    fun intArrayLoadStoreWorksAndDoesNotAffectRefCount() {
+        val mm = MemoryManager()
+        val ref = mm.newIntArray(3)
+
+        mm.intArrayStore(ref, 0, 42L)
+        mm.intArrayStore(ref, 1, -7L)
+
+        assertEquals(42L, mm.intArrayLoad(ref, 0))
+        assertEquals(-7L, mm.intArrayLoad(ref, 1))
+        assertEquals(0L, mm.intArrayLoad(ref, 2))
+
+        assertEquals(1, mm.debugRefCount(ref))
+
+        mm.release(ref)
+        assertEquals(0, mm.debugHeapObjectCount())
+    }
+
+    @Test
+    fun releaseUnknownHeapIdThrows() {
+        val mm = MemoryManager()
+        val bogus = Value.ArrayRef(999)
+
+        assertThrows<IllegalStateException> {
+            mm.release(bogus)
+        }
+    }
+
+    @Test
+    fun outOfBoundsThrows() {
+        val mm = MemoryManager()
+        val ref = mm.newIntArray(1)
+
+        assertThrows<IndexOutOfBoundsException> {
+            mm.intArrayLoad(ref, 1)
+        }
+
+        mm.release(ref)
+    }
+}
